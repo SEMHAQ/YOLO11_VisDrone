54a5ad0516de349bb08a5551352ba8dbbb45d87e
semhaq <2990280345@qq.com>
2025-11-12 23:50:26 +0800

    基本模型训练完成

--- train.py
+++ train.py
@@ -28,8 +28,8 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     model = YOLO('ultralytics/cfg/models/11/yolo11n.yaml') # YOLO11
-    # model.load('yolo11n.pt') # loading pretrain weights
-    model.train(data='/root/dataset/dataset_visdrone/data.yaml',
+    model.load('yolo11n.pt') # loading pretrain weights
+    model.train(data='dataset/data.yaml',
                 cache=False,
                 imgsz=640,
                 epochs=300,
