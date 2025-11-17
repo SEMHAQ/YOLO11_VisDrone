19ec611b32f9894f234d6c8ec5e5c215b562fa93
SEMHAQ <2990280345@qq.com>
2025-11-17 10:21:03 +0800

    update val-kitti

--- val.py
+++ val.py
@@ -19,14 +19,14 @@ def get_weight_size(path):
 if __name__ == '__main__':
     model_path = 'runs/train/GLSA-LAWDS/weights/best.pt'
     model = YOLO(model_path) # 选择训练好的权重路径
-    result = model.val(data='ultralytics/cfg/datasets/coco.yaml',
+    result = model.val(data='ultralytics/cfg/datasets/kitti.yaml',
                         split='val', # split可以选择train、val、test 根据自己的数据集情况来选择.
                         imgsz=640,
                         batch=16,
                         # iou=0.7,
                         # rect=False,
                         # save_json=True, # if you need to cal coco metrice
-                        project='runs/val-coco',
+                        project='runs/val-kitti',
                         name='GLSA-LAWDS',
                         )
     
