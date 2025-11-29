5b7c277d1431bd00c494bed2dc5bbd038d29e101
semhaq <2990280345@qq.com>
2025-11-29 23:55:51 +0800

    code update

--- val.py
+++ val.py
@@ -6,12 +6,6 @@ from prettytable import PrettyTable
 from ultralytics import YOLO
 from ultralytics.utils.torch_utils import model_info
 
-# BILIBILI UP 魔傀面具
-# 验证参数官方详解链接：https://docs.ultralytics.com/modes/val/#usage-examples:~:text=of%20each%20category-,Arguments%20for%20YOLO%20Model%20Validation,-When%20validating%20YOLO
-
-# 精度小数点保留位数修改问题可看<使用说明.md>下方的<YOLOV8源码常见疑问解答小课堂>第五点
-# 最终论文的参数量和计算量统一以这个脚本运行出来的为准
-
 def get_weight_size(path):
     stats = os.stat(path)
     return f'{stats.st_size / 1024 / 1024:.1f}'
